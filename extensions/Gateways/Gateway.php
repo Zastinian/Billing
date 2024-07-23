a7e2594870f7ee5e3cfb0a948fed44795aeb07b6
Zastinian <zastinian@gmail.com>
2024-07-22 18:11:09 -0600

    feat: Update lint

--- extensions/Gateways/Gateway.php
+++ extensions/Gateways/Gateway.php
@@ -11,17 +11,17 @@ interface Gateway {
     public static function seeder();
 
     public static function routes();
-    
+
     /**
      * Redirect the client to an external payment website
      */
     public static function checkout(Invoice $invoice, string $url);
-    
+
     /**
      * Verify if the payment is valid
      */
     public static function payment(Request $request, Invoice $invoice, string $url);
-    
+
     /**
      * Show the extension settings page in admin area
      */
