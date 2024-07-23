46f4a573a034941774b6aaab24058143beca01c8
Zastinian <zastinian@gmail.com>
2024-07-23 10:24:06 -0600

    feat: Update route

--- extensions/Gateways/PayPal/routes.php
+++ extensions/Gateways/PayPal/routes.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::any('/extension/paypal/ipn', 'Extensions\Gateways\PayPal\Controller@ipn')->name('paypal.ipn');
+Route::any('/paypal/ipn', 'Extensions\Gateways\PayPal\Controller@ipn')->name('paypal.ipn');
