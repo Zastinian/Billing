3f392e81909001bddba22c3460c49ccccd4a71a4
Zastinian <zastinian@gmail.com>
2024-07-22 18:10:49 -0600

    feat: Added ip

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -14,5 +14,7 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         // PayPal IPN
         '/extension/paypal/ipn',
+        // MercadoPago IPN
+        '/extension/mercadopago/ipn',
     ];
 }
