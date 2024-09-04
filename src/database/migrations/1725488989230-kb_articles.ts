71cd3a9c1035eaaccaf877817ffcc8552f4c5e61
Zastinian <zastinian@gmail.com>
2024-09-04 17:52:19 -0600

    feat: Added kb migrations

---
+++ src/database/migrations/1725488989230-kb_articles.ts
@@ -0,0 +1,55 @@
+import { type MigrationInterface, type QueryRunner, Table } from "typeorm";
+
+export class KbArticles1725488989230 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "kb_articles",
+                columns: [
+                    {
+                        name: "id",
+                        type: "integer",
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: "increment",
+                        unsigned: true,
+                    },
+                    {
+                        name: "category_id",
+                        type: "int",
+                        unsigned: true,
+                    },
+                    {
+                        name: "subject",
+                        type: "varchar",
+                        length: "255",
+                    },
+                    {
+                        name: "content",
+                        type: "text",
+                    },
+                    {
+                        name: "order",
+                        type: "int",
+                        unsigned: true,
+                        default: 1000,
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        isNullable: true,
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        isNullable: true,
+                    },
+                ],
+            }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("kb_articles");
+    }
+}
