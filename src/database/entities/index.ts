2f4ca32213ffb05aaeca9ae18c33a2729616a262
Zastinian <zastinian@gmail.com>
2024-09-09 17:47:55 -0600

    feat: Added export

---
+++ src/database/entities/index.ts
@@ -0,0 +1,51 @@
+import { Announcements } from "./Announcements";
+import { Categories } from "./Categories";
+import { Clients } from "./Clients";
+import { Coupons } from "./Coupons";
+import { Credits } from "./Credits";
+import { Currencies } from "./Currencies";
+import { Discounts } from "./Discounts";
+import { Emails } from "./Emails";
+import { FailedJobs } from "./FailedJobs";
+import { Invoices } from "./Invoices";
+import { Jobs } from "./Jobs";
+import { KbArticles } from "./KbArticles";
+import { KbCategories } from "./KbCategories";
+import { Logs } from "./Logs";
+import { Pages } from "./Pages";
+import { PasswordResets } from "./PasswordResets";
+import { PlanCycles } from "./PlanCycles";
+import { Plans } from "./Plans";
+import { Servers } from "./Servers";
+import { Settings } from "./Settings";
+import { Taxes } from "./Taxes";
+import { TicketContents } from "./TicketContents";
+import { Tickets } from "./Tickets";
+import { UsedCoupons } from "./UsedCoupons";
+
+export default [
+    Announcements,
+    Categories,
+    Clients,
+    Coupons,
+    Credits,
+    Currencies,
+    Discounts,
+    Emails,
+    FailedJobs,
+    Invoices,
+    Jobs,
+    KbArticles,
+    KbCategories,
+    Logs,
+    Pages,
+    PasswordResets,
+    PlanCycles,
+    Plans,
+    Servers,
+    Settings,
+    Taxes,
+    TicketContents,
+    Tickets,
+    UsedCoupons,
+];
