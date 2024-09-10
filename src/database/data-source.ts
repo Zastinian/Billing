f4b27bbf4b7758ff23559844dca4602fac18aa24
Zastinian <zastinian@gmail.com>
2024-09-09 18:08:53 -0600

    feat: Update data source

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,74 +1,75 @@
 import { DataSource } from "typeorm";
 
-if (!process.env.APP_KEY) {
+if (!import.meta.env.APP_KEY) {
     throw new Error("APP_KEY is not set, use `bun run --bun key:generate` to generate a new key");
 }
 
 import entities from "./entities";
+import migrations from "./migrations";
 
 const getDatabaseConfig = () => {
-    switch (process.env.DB_CONNECTION) {
+    switch (import.meta.env.DB_CONNECTION) {
         case "mariadb":
         case "mysql": {
             return {
-                type: process.env.DB_CONNECTION,
-                host: process.env.DB_HOST,
-                port: Number(process.env.DB_PORT || 3306),
-                username: process.env.DB_USERNAME,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_DATABASE,
+                type: import.meta.env.DB_CONNECTION,
+                host: import.meta.env.DB_HOST,
+                port: Number(import.meta.env.DB_PORT || 3306),
+                username: import.meta.env.DB_USERNAME,
+                password: import.meta.env.DB_PASSWORD,
+                database: import.meta.env.DB_DATABASE,
             };
         }
         case "postgres": {
             return {
-                type: process.env.DB_CONNECTION,
-                host: process.env.DB_HOST,
-                port: Number(process.env.DB_PORT || 5432),
-                username: process.env.DB_USERNAME,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_DATABASE,
+                type: import.meta.env.DB_CONNECTION,
+                host: import.meta.env.DB_HOST,
+                port: Number(import.meta.env.DB_PORT || 5432),
+                username: import.meta.env.DB_USERNAME,
+                password: import.meta.env.DB_PASSWORD,
+                database: import.meta.env.DB_DATABASE,
             };
         }
         case "cockroachdb": {
             return {
-                type: process.env.DB_CONNECTION,
-                host: process.env.DB_HOST,
-                port: Number(process.env.DB_PORT || 26257),
-                username: process.env.DB_USERNAME,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_DATABASE,
+                type: import.meta.env.DB_CONNECTION,
+                host: import.meta.env.DB_HOST,
+                port: Number(import.meta.env.DB_PORT || 26257),
+                username: import.meta.env.DB_USERNAME,
+                password: import.meta.env.DB_PASSWORD,
+                database: import.meta.env.DB_DATABASE,
                 timeTravelQueries: true,
             };
         }
         case "sqlite": {
             return {
-                type: process.env.DB_CONNECTION,
-                database: `${`${process.env.DB_DATABASE}`.includes(".sqlite") ? process.env.DB_DATABASE : `${process.env.DB_DATABASE}.sqlite`}`,
+                type: import.meta.env.DB_CONNECTION,
+                database: `${`${import.meta.env.DB_DATABASE}`.includes(".sqlite") ? import.meta.env.DB_DATABASE : `${import.meta.env.DB_DATABASE}.sqlite`}`,
             };
         }
         case "mssql": {
             return {
-                type: process.env.DB_CONNECTION,
-                host: process.env.DB_HOST,
-                port: Number(process.env.DB_PORT || 1433),
-                username: process.env.DB_USERNAME,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_DATABASE,
+                type: import.meta.env.DB_CONNECTION,
+                host: import.meta.env.DB_HOST,
+                port: Number(import.meta.env.DB_PORT || 1433),
+                username: import.meta.env.DB_USERNAME,
+                password: import.meta.env.DB_PASSWORD,
+                database: import.meta.env.DB_DATABASE,
             };
         }
         case "mongodb": {
             return {
-                type: process.env.DB_CONNECTION,
-                host: process.env.DB_HOST,
-                port: Number(process.env.DB_PORT || 27017),
-                username: process.env.DB_USERNAME,
-                password: process.env.DB_PASSWORD,
-                database: process.env.DB_DATABASE,
+                type: import.meta.env.DB_CONNECTION,
+                host: import.meta.env.DB_HOST,
+                port: Number(import.meta.env.DB_PORT || 27017),
+                username: import.meta.env.DB_USERNAME,
+                password: import.meta.env.DB_PASSWORD,
+                database: import.meta.env.DB_DATABASE,
             };
         }
         default:
             throw new Error(
-                `Unsupported DB_CONNECTION: ${process.env.DB_CONNECTION}, available connections: mariadb, mysql, postgres, cockroachdb, sqlite, mssql, mongodb`,
+                `Unsupported DB_CONNECTION: ${import.meta.env.DB_CONNECTION}, available connections: mariadb, mysql, postgres, cockroachdb, sqlite, mssql, mongodb`,
             );
     }
 };
@@ -78,13 +79,15 @@ const AppDataSource = new DataSource({
     synchronize: false,
     logging: false,
     entities: entities,
-    migrations: ["src/database/migrations/*.ts"],
+    migrations: migrations,
     subscribers: [],
 });
 
-await AppDataSource.initialize().catch(() => {
+await AppDataSource.initialize().catch((e) => {
     console.error("Unable to connect to the database, please check your configuration.");
-    console.error("Try using `bun run --bun db:migrate` to create the database tables.");
+    console.error("Try using `npm run db:migrate` to create the database tables.");
+
+    console.error(`Error: ${e}`);
     process.exit(1);
 });
 
