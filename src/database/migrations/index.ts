d9bb622c4901590800a408204e8932509b48f122
Zastinian <zastinian@gmail.com>
2024-09-09 18:06:26 -0600

    feat: Added export

---
+++ src/database/migrations/index.ts
@@ -0,0 +1,51 @@
+import { Announcements1725918171200 } from "./1725918171200-announcements";
+import { Categories1725918209833 } from "./1725918209833-categories";
+import { Clients1725918291066 } from "./1725918291066-clients";
+import { Coupons1725918310779 } from "./1725918310779-coupons";
+import { Credits1725918353070 } from "./1725918353070-credits";
+import { Currencies1725918367770 } from "./1725918367770-currencies";
+import { Discounts1725918389238 } from "./1725918389238-discounts";
+import { Emails1725918411118 } from "./1725918411118-emails";
+import { FailedJobs1725918466111 } from "./1725918466111-failed_jobs";
+import { Invoices1725918553654 } from "./1725918553654-invoices";
+import { Jobs1725918762227 } from "./1725918762227-jobs";
+import { KbArticles1725918780956 } from "./1725918780956-kb_articles";
+import { KbCategories1725918794433 } from "./1725918794433-kb_categories";
+import { Logs1725918955565 } from "./1725918955565-logs";
+import { Pages1725918990765 } from "./1725918990765-pages";
+import { PasswordResets1725919004832 } from "./1725919004832-password_resets";
+import { PlanCycles1725919024889 } from "./1725919024889-plan_cycles";
+import { Plans1725919112520 } from "./1725919112520-plans";
+import { Servers1725919156381 } from "./1725919156381-servers";
+import { Settings1725919183938 } from "./1725919183938-settings";
+import { Taxes1725919205001 } from "./1725919205001-taxes";
+import { TicketContents1725919218693 } from "./1725919218693-ticket_contents";
+import { Tickets1725919243975 } from "./1725919243975-tickets";
+import { UsedCoupons1725919262825 } from "./1725919262825-used_coupons";
+
+export default [
+    Announcements1725918171200,
+    Categories1725918209833,
+    Clients1725918291066,
+    Coupons1725918310779,
+    Credits1725918353070,
+    Currencies1725918367770,
+    Discounts1725918389238,
+    Emails1725918411118,
+    FailedJobs1725918466111,
+    Invoices1725918553654,
+    Jobs1725918762227,
+    KbArticles1725918780956,
+    KbCategories1725918794433,
+    Logs1725918955565,
+    Pages1725918990765,
+    PasswordResets1725919004832,
+    PlanCycles1725919024889,
+    Plans1725919112520,
+    Servers1725919156381,
+    Settings1725919183938,
+    Taxes1725919205001,
+    TicketContents1725919218693,
+    Tickets1725919243975,
+    UsedCoupons1725919262825,
+];
