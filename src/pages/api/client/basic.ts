40245946bbaf8426ddc0ec0d6c07b05adf249f58
Zastinian <zastinian@gmail.com>
2024-10-14 18:35:45 -0600

    feat: Update client email & basic apis

--- src/pages/api/client/basic.ts
+++ src/pages/api/client/basic.ts
@@ -1,32 +1,39 @@
-import { settings, clients } from "@/database/index";
+import { clients } from "@/database/index";
 import profile from "@/utils/profile";
 import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ cookies, request, redirect, rewrite }) => {
-  const storeUrl = new URL(
-    (await settings.findOneBy({ key: "store_url" }).then((storeUrl) => storeUrl?.value)) ?? "",
-  );
+  const storeUrl = new URL(import.meta.env.STORE_URL ?? "");
   const requestUrl = new URL(request.url);
   if (requestUrl.origin !== storeUrl.origin) {
     return rewrite("/404");
   }
   const cookie: string = `${cookies.get("_SECURE_SESSION_TOKEN_")?.value}`;
   const c = profile(cookie);
-
   if (c.success === true && c.clientId !== null) {
-    const client = await clients.findOneBy({ id: c.clientId }).then((client) => client);
+    const client = await clients.findOneBy({ id: c.clientId });
     if (client?.email !== c.email) {
       return redirect("/");
     }
+    if (client.sessionToken !== c.sessionToken) {
+      return redirect("/");
+    }
     const data = Object.fromEntries(new URLSearchParams(await request.text()));
-    if (!data.currency || !data.country) {
-      return redirect("/client/settings?type=error&msg=client.settings.basic.error");
+    if (
+      !data.currency ||
+      !data.auto_renew ||
+      typeof Number(data.auto_renew) !== "number" ||
+      Number.isNaN(Number(data.auto_renew)) ||
+      Number(data.auto_renew) < 0 ||
+      Number(data.auto_renew) > 1
+    ) {
+      return redirect("/client/settings?type=danger&msg=client.settings.basic.error");
     }
     client.currency = Number(data.currency);
-    client.country = Number(data.country);
+    client.autoRenew = Number(data.auto_renew);
+    client.updatedAt = new Date();
     await clients.save(client);
     return redirect("/client/settings?type=success&msg=client.settings.success");
   }
-
   return redirect("/");
 };
--- src/pages/api/client/email.ts
+++ src/pages/api/client/email.ts
@@ -1,40 +1,119 @@
 import jwt from "jsonwebtoken";
-import { settings, clients, jobs } from "@/database/index";
-import { Jobs } from "@/database/entities/Jobs";
+import { clients, settings } from "@/database/index";
 import profile from "@/utils/profile";
 import type { APIRoute } from "astro";
 
 export const POST: APIRoute = async ({ cookies, request, redirect, rewrite }) => {
-  const storeUrl = new URL(
-    (await settings.findOneBy({ key: "store_url" }).then((storeUrl) => storeUrl?.value)) ?? "",
-  );
+  const storeUrl = new URL(import.meta.env.STORE_URL ?? "");
   const requestUrl = new URL(request.url);
   if (requestUrl.origin !== storeUrl.origin) {
     return rewrite("/404");
   }
   const cookie: string = `${cookies.get("_SECURE_SESSION_TOKEN_")?.value}`;
   const c = profile(cookie);
-
   if (c.success === true && c.clientId !== null) {
-    const client = await clients.findOneBy({ id: c.clientId }).then((client) => client);
+    const client = await clients.findOneBy({ id: c.clientId });
     if (client?.email !== c.email) {
+      return redirect("/");
+    }
+    if (client.sessionToken !== c.sessionToken) {
       return redirect("/");
     }
     const data = Object.fromEntries(new URLSearchParams(await request.text()));
     if (!data.email || !data.password) {
-      return redirect("/client/settings?type=error&msg=client.settings.email.error");
+      return redirect("/client/settings?type=danger&msg=client.settings.email.error");
     }
-    if (!client.verifyPassword(data.password)) {
-      return redirect("/client/settings?type=error&msg=client.settings.email.error");
+    if (!(await client.verifyPassword(data.password))) {
+      return redirect("/client/settings?type=danger&msg=client.settings.email.error");
     }
-    const newJob = new Jobs();
-    newJob.queue = "update_client_email";
-    newJob.payload = JSON.stringify({
-      old: client.email,
-      new: data.email,
-    });
-    await jobs.save(newJob);
+    // const newJob = new Jobs();
+    // newJob.queue = "update_client_email";
+    // newJob.payload = JSON.stringify({
+    //   old: client.email,
+    //   new: data.email,
+    // });
+    // await jobs.save(newJob);
+    const checkClient = await clients.findOneBy({ email: data.email });
+    if (checkClient) {
+      return redirect("/client/settings?type=danger&msg=client.settings.email.already");
+    }
+    const panelUrl = await settings
+      .findOneBy({ key: "panel_url" })
+      .then((panelUrl) => panelUrl?.value);
+    if (!panelUrl) {
+      return redirect("/client/settings?type=danger&msg=error");
+    }
+    const panelAppApiKey = await settings
+      .findOneBy({ key: "panel_app_api_key" })
+      .then((panelAppApiKey) => panelAppApiKey?.value);
+    if (!panelAppApiKey) {
+      return redirect("/client/settings?type=danger&msg=error");
+    }
+    const checkPterodactylUser = await fetch(
+      new URL(`/api/application/users?filter[email]=${data.email}`, panelUrl).toString(),
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${panelAppApiKey}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    if (checkPterodactylUser.status !== 200) {
+      return redirect("/client/settings?type=danger&msg=error");
+    }
+    if (!checkPterodactylUser.ok) {
+      return redirect("/client/settings?type=danger&msg=error");
+    }
+    const dataPterodactylUser = await checkPterodactylUser.json();
+    if (dataPterodactylUser.data.length > 0) {
+      return redirect("/client/settings?type=danger&msg=client.settings.email.already");
+    }
+    const getPterodactylUser = await fetch(
+      new URL(`/api/application/users/${client.userId}`, panelUrl).toString(),
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${panelAppApiKey}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      },
+    );
+    if (getPterodactylUser.status !== 200) {
+      return redirect("/client/settings?type=danger&msg=error");
+    }
+    if (!getPterodactylUser.ok) {
+      return redirect("/client/settings?type=danger&msg=error");
+    }
+    const getPterodactylUserData = await getPterodactylUser.json();
+    const pterodactyl = await fetch(
+      new URL(`/api/application/users/${client.userId}`, panelUrl).toString(),
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${panelAppApiKey}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: data.email,
+          username: getPterodactylUserData.attributes.username,
+          first_name: getPterodactylUserData.attributes.first_name,
+          last_name: getPterodactylUserData.attributes.last_name,
+        }),
+      },
+    );
+    if (pterodactyl.status !== 200) {
+      return redirect("/client/settings?type=danger&msg=error");
+    }
+    if (!pterodactyl.ok) {
+      return redirect("/client/settings?type=danger&msg=error");
+    }
     client.email = data.email;
+    client.setSessionToken();
+    client.updatedAt = new Date();
     await clients.save(client);
     const maxAge = 7 * 24 * 60 * 60 * 1000;
     const expire = Math.floor(Date.now() / 1000) + maxAge;
@@ -43,6 +122,7 @@ export const POST: APIRoute = async ({ cookies, request, redirect, rewrite }) =>
         exp: expire,
         clientId: client.id,
         email: client.email,
+        sessionToken: client.sessionToken,
       },
       import.meta.env.APP_KEY,
     );
@@ -53,6 +133,5 @@ export const POST: APIRoute = async ({ cookies, request, redirect, rewrite }) =>
     });
     return redirect("/client/settings?type=success&msg=client.settings.success");
   }
-
   return redirect("/");
 };
