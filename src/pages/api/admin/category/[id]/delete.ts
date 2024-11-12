fd6cd5eb174c618937f737cce5f1a193c28b7c5d
Zastinian <zastinian@gmail.com>
2024-11-12 17:45:40 -0600

    feat: Added delete category

---
+++ src/pages/api/admin/category/[id]/delete.ts
@@ -0,0 +1,49 @@
+import { clients, plans } from "@/database/index";
+import { categories } from "@/database/index";
+import profile from "@/utils/profile";
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ cookies, request, redirect, rewrite, params }) => {
+  const storeUrl = new URL(import.meta.env.STORE_URL ?? "");
+  const requestUrl = new URL(request.url);
+  if (requestUrl.origin !== storeUrl.origin) {
+    return rewrite("/404");
+  }
+  const cookie: string = `${cookies.get("_SECURE_SESSION_TOKEN_")?.value}`;
+  const c = profile(cookie);
+
+  if (c.success === true && c.clientId !== null) {
+    const client = await clients.findOneBy({ id: c.clientId });
+    if (client?.email !== c.email) {
+      return redirect("/");
+    }
+    if (client?.sessionToken !== c.sessionToken) {
+      return redirect("/");
+    }
+    if (client?.isAdmin !== 1) {
+      return redirect("/");
+    }
+    const categoryId = params.id;
+    if (!categoryId || typeof Number(categoryId) !== "number" || Number.isNaN(Number(categoryId))) {
+      return redirect("/admin/categories?type=danger&msg=admin.category.not_found");
+    }
+    const category = await categories.findOneBy({ id: Number(categoryId) });
+    if (!category) {
+      return redirect("/admin/categories?type=danger&msg=admin.category.not_found");
+    }
+    const plansData = await plans.find({
+      where: { categoryId: category.id },
+      select: ["id", "categoryId"],
+    });
+    if (plansData.length > 0) {
+      return redirect(
+        `/admin/categories/${category.id}?type=danger&msg=admin.category.delete.error`,
+      );
+    }
+    await categories.remove(category);
+    return redirect(
+      `/admin/categories?type=success&msg=admin.category.delete.success`,
+    );
+  }
+  return redirect("/");
+};
