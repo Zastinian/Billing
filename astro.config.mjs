f88d0481835d30b79a6f74a9484fde12a982889d
Zastinian <zastinian@gmail.com>
2024-12-04 17:12:28 -0600

    feat: Change node to bun

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
+// @ts-check
 import { defineConfig } from "astro/config";
-
-import node from "@astrojs/node";
+import bun from "@hedystia/astro-bun";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: bun(),
   server: {
     host: "0.0.0.0",
     port: 3000,
