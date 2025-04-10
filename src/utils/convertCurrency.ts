04a7b6f85e2980cba2e0164608ab8cb176748e24
Zastinian <zastinian@gmail.com>
2025-04-09 20:21:46 -0600

    feat: Added function

--- src/utils/convertCurrency.ts
+++ src/utils/convertCurrency.ts
@@ -1,6 +1,10 @@
 import type { Currencies } from "@/database/entities/Currencies";
 
-export default (amount: number, fromCurrency: Currencies, toCurrency: Currencies): string => {
+export default function convertCurrency(
+  amount: number,
+  fromCurrency: Currencies,
+  toCurrency: Currencies,
+): string {
   const convertedAmount = (amount * toCurrency.rate) / fromCurrency.rate;
   return convertedAmount.toFixed(toCurrency.precision);
-};
+}
