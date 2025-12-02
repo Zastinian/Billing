081cad60e70ada81e4be17cc2088f85bbea49e8d
Zastinian <zastinian@gmail.com>
2025-12-02 13:59:20 -0600

    feat: Update i18n

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,4 +1,4 @@
-import { type FlattenedUiType, defaultLang, flattenedUi, ui } from "./ui";
+import { defaultLang, type FlattenedUiType, flattenedUi, type ui } from "./ui";
 
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: keyof FlattenedUiType) {
